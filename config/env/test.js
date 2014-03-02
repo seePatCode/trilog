4b3f9649c46f64ae1c08082159b801ce854430f7
Patrick Cullen <pat@seepatcode.com>
2014-03-02 18:21:52 +0000

    Latest stylings to make it look more like trilog and less like mean

--- config/env/test.js
+++ config/env/test.js
@@ -4,7 +4,7 @@ module.exports = {
     db: 'mongodb://localhost/mean-test',
     port: 3001,
     app: {
-        name: 'MEAN - A Modern Stack - Test'
+        name: 'trilog - Triathlon Training Log Made Simple'
     },
     facebook: {
         clientID: 'APP_ID',
